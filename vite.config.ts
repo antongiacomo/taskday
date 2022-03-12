3f5f4fd322c6bd3f8f151dee2f66185abd261cbf
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-03-12 19:32:25 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import { defineConfig } from "vite";
+import Components from "unplugin-vue-components/vite";
+import laravel from "vite-plugin-laravel";
+import resolveExternalsPlugin from "vite-plugin-resolve-externals";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      plugins: [
+        resolveExternalsPlugin({
+          vue: "Vue",
+        }),
+      ],
+    },
+  },
+  plugins: [
+    vue(),
+    Components({
+      dirs: [
+        path.resolve(__dirname, "../taskday-framework/resources/components"),
+      ],
+      extensions: ["vue"],
+      deep: true,
+    }),
+    laravel({
+      postcss: [
+        require("postcss-import"),
+        require("tailwindcss/nesting"),
+        require("tailwindcss"),
+        require("autoprefixer"),
+      ],
+    }),
+  ],
+});
